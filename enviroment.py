0105497af0ae1a28be8fb6f43695fe1d0fea0cc5
alexboyd92 <alexboyd92@github.com>
2019-11-10 19:42:40 -0500

    Co-authored-by: stevenhawes <stevenmhawes@gmail.com> Co-authored-by: Neil Macklin-Camel <neil.macklin-camel@wmich.edu>

--- enviroment.py
+++ enviroment.py
@@ -1,2 +1,36 @@
 import os
 import numpy
+import random
+
+
+class environment:
+    def __init__(self, x=1, y=2, walls=0):
+        self.enviro = []
+        self.setSize(x,y)
+        self.setShape(walls)
+        self.setDirt()
+
+        
+    def setSize(self,x=1, y=2):
+        # x is rows y is column  
+        self.enviro = [ [0] * y for _ in range(x)]
+
+    def setShape(self, walls=0):  
+        pass 
+    
+    def setDirt(self):
+        for rowIndex, row in enumerate(self.enviro):
+            for colIndex, col in enumerate(row):
+                rand = random.randint(0,1)
+                # places dirt if rand = 1, clean if 0
+                if rand == 1:
+                    self.enviro[rowIndex][colIndex] = rand
+
+    def print_shape(self):
+        print(self.enviro)
+        
+
+
+env = environment()
+
+env.print_shape()
