7ac95e3d03084fb356cb8ab0cd69aa3604c619e3
stevenhawes <stevenmhawes@gmail.com>
2019-11-13 16:33:49 -0500

    changes

--- enviroment.py
+++ enviroment.py
@@ -1,30 +1,28 @@
 import os
-import numpy
+import numpy as np
 import random
 
 
 class environment:
     def __init__(self, x=1, y=2, walls=0):
         self.enviro = []
-        self.setSize(x,y)
-        self.setShape(walls)
-        self.setDirt()
+        self.x = 0
+        self.y = 0
+        self._setSize(x,y)
+        self._setShape(walls)
+        self._setDirt()
 
         
-    def setSize(self,x=1, y=2):
-        # x is rows y is column  
-        self.enviro = [ [0] * y for _ in range(x)]
+    def _setSize(self,x=1, y=2):
+        self.x = x
+        self.y = y
 
-    def setShape(self, walls=0):  
+    def _setShape(self, walls=0):  
         pass 
     
-    def setDirt(self):
-        for rowIndex, row in enumerate(self.enviro):
-            for colIndex, col in enumerate(row):
-                rand = random.randint(0,1)
-                # places dirt if rand = 1, clean if 0
-                if rand == 1:
-                    self.enviro[rowIndex][colIndex] = rand
+    def _setDirt(self):
+        # sets dirt placement (1 = dirt at that location), randomized
+        self.enviro = np.floor(2*np.random.random((self.x,self.y)))
 
     def print_shape(self):
         print(self.enviro)
