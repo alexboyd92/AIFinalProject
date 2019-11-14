3bb8bf9d4d53aa1d7971ed4edf8c59a943804ec0
stevenhawes <stevenmhawes@gmail.com>
2019-11-14 14:32:51 -0500

    update

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from enviroment import Environment
 from agent import SimpleReflexAgent
 
-env = Environment(3,3)
+scoreTotal = 0
+
+env = Environment(2, 2)
 print('Environment:')
 env.print_shape()
 print('Initial Location:', end=' ')
 vacuum = SimpleReflexAgent(env)
 print('Vacuum Start:\n')
 vacuum.runVacuum()
+
+
